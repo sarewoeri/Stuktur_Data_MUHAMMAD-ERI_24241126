d66fc27a5292986b740cdc5d850fa5c71f9833f2
sarewoeri <muhammaderierieri2004@gmail.com>
2025-04-22 20:30:28 +0900

    Add files via upload

---
+++ strukturdata.py
@@ -0,0 +1,27 @@
+# operasi logika
+#or and xor not
+
+x = True
+z = not X
+print ("nilai dari z =", z)
+
+print ("**** and ****")
+x = True
+y = True
+z = x and y
+print(x,'and', y, '=', z)
+
+x = True
+y = False
+z = x and y
+print (x, 'and', y, '=', z)
+
+x = False
+y = True
+z = x and y
+print (x, 'and', y,'=', z)
+
+x = False
+y = False
+z = x and y
+print (x, 'and', y, '=',z)
