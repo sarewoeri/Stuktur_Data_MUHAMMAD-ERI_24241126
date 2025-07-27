4c665bbdb73c6e2418e03d0eb42c02034fd5d585
sarewoeri <muhammaderierieri2004@gmail.com>
2025-07-27 19:04:46 +0800

    Add files via upload

---
+++ tugas3.py
@@ -0,0 +1,36 @@
+#percabangan
+#beli lambo = 1000
+
+nilai = int(input("masukan nilai perkuliahan "))
+
+if nilai >= 85:
+    print("Anda mendapatkan nilai A")
+
+elif nilai  >= 80:
+    print("Anda mendapatkan nilai A-")
+
+elif nilai  >= 75:
+    print("Anda mendapatkan nilai B+")
+
+elif nilai  >= 70:
+    print("Anda mendapatkan nilai B")
+
+elif nilai  >= 65:
+    print("Anda mendapatkan nilai B-")
+
+elif nilai  >= 60:
+    print("Anda mendapatkan nilai C+")
+
+elif nilai  >= 55:
+    print("Anda mendapatkan nilai C")
+
+elif nilai  >= 50:
+    print("Anda mendapatkan nilai C-")
+
+elif nilai  >= 40:
+    print("Anda mendapatkan nilai D")
+
+else:  
+    print("Anda tidak lulus E")
+
+print("akhir dari program")
