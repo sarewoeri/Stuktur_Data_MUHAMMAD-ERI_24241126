62360c0a168a1d91337b822d1d3f2c1b6e14387b
sarewoeri <muhammaderierieri2004@gmail.com>
2025-07-27 19:06:32 +0800

    Add files via upload

---
+++ tugas7.py
@@ -0,0 +1,9 @@
+Meminta input dari pengguna
+domain = input("Masukkan nama domain anda: ")
+
+# Memisahkan nama dan domain
+nama, domain = domain.split('.')
+
+# Menampilkan hasil
+print("Nama domain anda:", Muhammad Eri)
+print("Domain yang anda gunakan:",Bima)
