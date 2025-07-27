2bd99f6bf9a150dd0d8ebd60235fa3480db9a32e
sarewoeri <muhammaderierieri2004@gmail.com>
2025-07-27 19:05:29 +0800

    Add files via upload

---
+++ tugas4.py
@@ -0,0 +1,14 @@
+#buatlah program pengecekan umur
+umur = int(input("masukan umur : "))
+if umur < 18 :
+    print("anda masih anak-anak")
+elif umur > 18:
+    print("anda sudah dewasa")
+else:
+    print("umur anda tepat 18 tahun")
+
+
+#mengetahui umur
+umur = 19
+nilai = ("anak anak" if umur <=18 >=18 else "dewasa")
+print (nilai )
