64d934d284e473548bc3387a5fbc6377f4a19143
sarewoeri <muhammaderierieri2004@gmail.com>
2025-07-27 19:06:54 +0800

    Add files via upload

---
+++ tugas8.py
@@ -0,0 +1,20 @@
+# ARITMATIKA (X + - ** % // )
+
+a = 24 
+b = 26 
+
+# penjumlahan 
+hasil = 
+print (" nilai dari    +   = " , hasil )
+
+# perpangkatan 
+hasil    +
+print (" nilai dari a pankat b = " , hasil )
+
+# modulus ( sisa bagi )
+hasil a % b 
+print (" nilai dari a mod b ", hasil )
+
+# foloor dividion ( pembagian terdekat )
+hasil a // b 
+print (" nilai dari a flor b = " hasil )
