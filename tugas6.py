efd045fc550fc7fa5e95f99d762ec872d66a15f9
sarewoeri <muhammaderierieri2004@gmail.com>
2025-07-27 19:06:10 +0800

    Add files via upload

---
+++ tugas6.py
@@ -0,0 +1,14 @@
+# Variabel awal
+number = "1234567890"
+
+# 1. Menampilkan data terakhir
+data_terakhir = number[-1]
+print("Data terakhir:", data_terakhir)
+
+# 2. Menampilkan data pertama
+data_pertama = number[0]
+print("Data pertama:", data_pertama)
+
+# 3. Menampilkan data ke-3 sampai ke-6
+data_ke3_ke6 = number[2:6]  # Index ke-2 sampai ke-5
+print("Data ke-3 sampai ke-6:", data_ke3_ke6)
